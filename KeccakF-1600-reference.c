990b3af3de05721ce60e9bcac5a4f7119df8a15f
Nathaniel Graff <nathaniel.graff@gmail.com>
2016-12-24 19:00:30 -0800

    Fixing more code style

--- KeccakF-1600-reference.c
+++ KeccakF-1600-reference.c
@@ -32,8 +32,11 @@ void KeccakInitializeRhoOffsets();
 void KeccakXorDataIntoState(unsigned char *state, const unsigned char *data, unsigned int dataLengthInBytes);
 
 // Internal logic
-#define index(x, y) (((x)%5)+5*((y)%5))
-#define ROL64(a, offset) ((offset != 0) ? ((((uint64_t)a) << offset) ^ (((uint64_t)a) >> (64-offset))) : a)
+#define index(x, y) ( ((x) % 5) + 5 * ((y) % 5) )
+
+uint64_t ROL64(uint64_t a, unsigned int offset) {
+    return ( ((uint64_t) a) << offset ) | ( ((uint64_t) a) >> (64 - offset) );
+}
 
 void theta(uint64_t *A);
 void rho(uint64_t *A);
@@ -60,14 +63,17 @@ int LFSR86540(uint8_t *LFSR)
 void KeccakInitializeRoundConstants()
 {
     uint8_t LFSRstate = 0x01;
-    unsigned int i, j, bitPosition;
-
-    for(i=0; i<nrRounds; i++) {
+    unsigned int bitPosition;
+    
+    unsigned int i, j;
+    for(i = 0; i < nrRounds; i++) {
         KeccakRoundConstants[i] = 0;
-        for(j=0; j<7; j++) {
-            bitPosition = (1<<j)-1; //2^j-1
+
+        for(j = 0; j < 7; j++) {
+            bitPosition = (1 << j) - 1; // 2^j - 1
+
             if (LFSR86540(&LFSRstate)) {
-                KeccakRoundConstants[i] ^= (uint64_t)1<<bitPosition;
+                KeccakRoundConstants[i] ^= (uint64_t) 1 << bitPosition;
             }
         }
     }
@@ -75,15 +81,20 @@ void KeccakInitializeRoundConstants()
 
 void KeccakInitializeRhoOffsets()
 {
-    unsigned int x, y, t, newX, newY;
+    unsigned int x, y, newX, newY;
 
     KeccakRhoOffsets[index(0, 0)] = 0;
+
     x = 1;
     y = 0;
-    for(t=0; t<24; t++) {
-        KeccakRhoOffsets[index(x, y)] = ((t+1)*(t+2)/2) % 64;
-        newX = (0*x+1*y) % 5;
-        newY = (2*x+3*y) % 5;
+
+    unsigned int t;
+    for(t = 0; t < 24; t++) {
+        KeccakRhoOffsets[index(x, y)] = ((t + 1) * (t + 2)/2) % 64;
+
+        newX = (0 * x + 1 * y) % 5;
+        newY = (2 * x + 3 * y) % 5;
+
         x = newX;
         y = newY;
     }
@@ -103,7 +114,7 @@ void KeccakXorDataIntoState(unsigned char *state, const unsigned char *data, uns
 {
     unsigned int i;
 
-    for(i=0; i<dataLengthInBytes; i++)
+    for(i = 0; i < dataLengthInBytes; i++)
     {
         state[i] ^= data[i];
     }
@@ -113,11 +124,11 @@ void KeccakPermutation(unsigned char *state)
 {
     unsigned int round;
     for(round = 0; round < nrRounds; round++) {
-        theta((uint64_t*)state);
-        rho((uint64_t*)state);
-        pi((uint64_t*)state);
-        chi((uint64_t*)state);
-        iota((uint64_t*)state, round);
+        theta((uint64_t*) state);
+        rho((uint64_t*) state);
+        pi((uint64_t*) state);
+        chi((uint64_t*) state);
+        iota((uint64_t*) state, round);
     }
 }
 
@@ -135,17 +146,17 @@ void theta(uint64_t *A)
     unsigned int x, y;
     uint64_t C[5], D[5];
 
-    for(x=0; x<5; x++) {
+    for(x = 0; x < 5; x++) {
         C[x] = 0; 
-        for(y=0; y<5; y++) {
+        for(y = 0; y < 5; y++) {
             C[x] ^= A[index(x, y)];
         }
     }
-    for(x=0; x<5; x++) {
-        D[x] = ROL64(C[(x+1)%5], 1) ^ C[(x+4)%5];
+    for(x = 0; x < 5; x++) {
+        D[x] = ROL64(C[(x + 1) % 5], 1) ^ C[(x + 4) % 5];
     }
-    for(x=0; x<5; x++) {
-        for(y=0; y<5; y++) {
+    for(x = 0; x < 5; x++) {
+        for(y = 0; y < 5; y++) {
             A[index(x, y)] ^= D[x];
         }
     }
@@ -155,8 +166,10 @@ void rho(uint64_t *A)
 {
     unsigned int x, y;
 
-    for(x=0; x<5; x++) for(y=0; y<5; y++) {
-        A[index(x, y)] = ROL64(A[index(x, y)], KeccakRhoOffsets[index(x, y)]);
+    for(x = 0; x < 5; x++) {
+        for(y = 0; y < 5; y++) {
+            A[index(x, y)] = ROL64(A[index(x, y)], KeccakRhoOffsets[index(x, y)]);
+        }
     }
 }
 
@@ -165,11 +178,15 @@ void pi(uint64_t *A)
     unsigned int x, y;
     uint64_t tempA[25];
 
-    for(x=0; x<5; x++) for(y=0; y<5; y++) {
-        tempA[index(x, y)] = A[index(x, y)];
+    for(x = 0; x < 5; x++) {
+        for(y = 0; y < 5; y++) {
+            tempA[index(x, y)] = A[index(x, y)];
+        }
     }
-    for(x=0; x<5; x++) for(y=0; y<5; y++) {
-        A[index(0*x+1*y, 2*x+3*y)] = tempA[index(x, y)];
+    for(x = 0; x < 5; x++) {
+        for(y = 0; y < 5; y++) {
+            A[index(0 * x + 1 * y, 2 * x + 3 * y)] = tempA[index(x, y)];
+        }
     }
 }
 
@@ -178,11 +195,11 @@ void chi(uint64_t *A)
     unsigned int x, y;
     uint64_t C[5];
 
-    for(y=0; y<5; y++) { 
-        for(x=0; x<5; x++) {
-            C[x] = A[index(x, y)] ^ ((~A[index(x+1, y)]) & A[index(x+2, y)]);
+    for(y = 0; y < 5; y++) { 
+        for(x = 0; x < 5; x++) {
+            C[x] = A[index(x, y)] ^ ((~A[index(x + 1, y)]) & A[index(x + 2, y)]);
         }
-        for(x=0; x<5; x++) {
+        for(x = 0; x < 5; x++) {
             A[index(x, y)] = C[x];
         }
     }
