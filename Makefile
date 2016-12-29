aaa2ee7a2093f0afb7006f39df91591fcf863d07
Nathaniel Graff <nathaniel.graff@gmail.com>
2016-12-29 09:56:54 -0800

    Basic fuzzing with AFL for whole-byte inputs
    Results of fuzzing
    ------------------
    cycles_done       : 115
    execs_done        : 3289355
    paths_total       : 18
    paths_favored     : 3
    paths_found       : 17
    max_depth         : 10
    stability         : 100.00%
    bitmap_cvg        : 0.16%
    unique_crashes    : 0
    unique_hangs      : 0

--- Makefile
+++ Makefile
@@ -5,13 +5,22 @@ KECCAK_LIB_C = KeccakF-1600-reference.c KeccakSponge.c KeccakNISTInterface.c
 KECCAK_LIB_H = KeccakF-1600-reference.h KeccakSponge.h KeccakNISTInterface.h
 KECCAK_LIB = $(KECCAK_LIB_C) $(KECCAK_LIB_H)
 
-all: build run
+all: mainReference run
 
-build: mainReference.c $(KECCAK_LIB_C) $(KECCAK_LIB_H)
+build: mainReference mainFuzz
+
+mainReference: mainReference.c $(KECCAK_LIB)
 	gcc mainReference.c $(KECCAK_LIB_C) -o mainReference $(COMPILER_FLAGS)
 
+mainFuzz: mainFuzz.c $(KECCAK_LIB)
+	afl-clang mainFuzz.c $(KECCAK_LIB_C) -o mainFuzz $(COMPILER_FLAGS)
+
+fuzz: mainFuzz afl-input
+	afl-fuzz -i afl-input -o afl-findings -t 2000 ./mainFuzz @@
+
 clean:
-	rm mainReference
+	rm mainReference mainFuzz
 
 run: mainReference
 	./mainReference
+
---
+++ mainFuzz.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 Nathaniel Graff
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "KeccakNISTInterface.h"
+
+#define hashBitLen 256
+
+int main(int argc, char * argv[]) {
+	
+	// Check if we've been passed an input file
+	if(argc < 2) {
+		printf("Nothing to do\n");
+		return 0;
+	}
+
+	// Open the input file
+	FILE * inputFile;
+	char inputFileName[100];
+	strncpy(inputFileName, argv[1], 100);
+	if((inputFile = fopen(inputFileName, "r")) == NULL) {
+		printf("Error opening %s\n", inputFileName);
+		return 1;
+	}
+
+	// Get the length of the file in bytes
+	struct stat inputFileStat;
+	stat(inputFileName, &inputFileStat);
+	uint32_t inputFileSize = inputFileStat.st_size;
+
+	printf("Input file has %d bytes\n", inputFileSize);
+
+	// Read the input data from the file
+	uint8_t * inputData;
+	inputData = calloc(inputFileSize, sizeof(uint8_t));
+
+	fread(inputData, sizeof(uint8_t), inputFileSize, inputFile);
+
+	// Hash the input data
+	uint8_t outputData[hashBitLen];
+	Hash(hashBitLen, inputData, 8*inputFileSize, outputData);
+
+	// Print the hash of the input data
+	char outputBuf[2*hashBitLen/8 + 1];
+
+	char temp[10];
+    uint32_t i;
+    for(i = 0; i < (hashBitLen/8); i++)
+    {
+        sprintf(temp, "%02x", outputData[i]);
+        strncpy((outputBuf+(2*i)), temp, 2);
+    }
+    outputBuf[2*hashBitLen/8] = 0;
+
+    printf("%s\n", outputBuf);
+
+	return 0;
+}
